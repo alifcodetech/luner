50137f3d86ae543b4a4acb50d12a8e0ea540486f
Abdul Khaliq <108611214+Abdulkhaliq9@users.noreply.github.com>
2025-10-01 12:23:18 -0700

    add cdn

--- user/header.php
+++ user/header.php
@@ -5,5 +5,6 @@
   <title>Plans - Alif Invest</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
